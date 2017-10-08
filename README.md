f810dd6eedac9538bc90622f476b56cdd162e1a9
Swaraj Panigrahi <swarajpanigrahi@Swarajs-MacBook-Pro.local>
2017-10-09 00:00:41 +0530

    modified readme

---
+++ README.md
@@ -0,0 +1,48 @@
+# List Creator
+
+## What it does?
+    This app takes a file, parses it and gives user a file with list of 
+    items for all hashtags. 
+    
+    Example 
+        *********
+            Saga #referlatter
+
+            Code be ur document
+            Why did u update #refer
+
+
+            REUSABLE COMPONENTS
+
+            REDUX-SAGA
+            *codebrahma
+
+
+            HR: rupesh.
+
+            6-in-1
+            What the flex box? #refer
+            necolas/React-native-web > for web #google
+        *********
+
+        O/P:
+            REFER
+                1. Why did u update
+                2. What the flex box?
+            
+            GOOGLE
+                1. necolas/React-native-web > for web
+
+    App would take one input.
+        1. File where notes was taken
+    
+    App would give the following.
+        1. File with the above output
+
+## HOW TO USE
+
+### FIRST TIME 
+
+`npm install`
+
+`node app.js -p path\to\file`
--- app.js
+++ app.js
@@ -6,47 +6,6 @@ const { parse } = require('./src/list-parser.js');
 
 const log = console.log;
 
-/* 
-    This app takes a file, prases it and gives user a file with list of 
-    items under hashtags. 
-    
-    Example 
-        *********
-            Saga #referlatter
-
-            Code be ur document
-            Why did u update #refer
-
-
-            REUSABLE COMPONENTS
-
-            REDUX-SAGA
-            *codebrahma
-
-
-            HR: rupesh.
-
-            6-in-1
-            What the flex box? #refer
-            necolas/React-native-web > for web #google
-        *********
-
-        O/P:
-            REFER
-                1. Why did u update
-                2. What the flex box?
-            
-            GOOGLE
-                1. necolas/React-native-web > for web
-
-    App would take one input.
-        1. File where notes was taken
-    
-    App would give the following.
-        1. File with the above output
-
-    `node app.js -p path\to\file`
-*/
 let cpu = {
     totalPercent: 100,
     used: 5,
